80dc8d79e3082bb30ea0d9fe56ad42940ca508e8
Shruti702 <karunakaramshruti@gmail.com>
2026-02-12 21:16:14 +0530

    Backend: Defined User.js model with fields for username, email, password, and avatar.

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Connects to MongoDB using the URI provided in the config.
 mongoose.connect(MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+//Define the base endpoints for the API.
 app.use("/api/auth", authRoutes);
 app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/channels", channelRoutes);
 
+//Tells the app to listen on the specified port for incoming connections.
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

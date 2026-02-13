73576791d0da22b86f0f21b6c4e1be6301350d82
Shruti702 <karunakaramshruti@gmail.com>
2026-02-13 18:30:48 +0530

    Frontend: Updated App.jsx and main.jsx.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react"; 
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar"; 
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import VideoPlayer from "./pages/VideoPlayer";
+import Channel from "./pages/Channel";
+
+//Updates Layout to manage Sidebar state.
+const Layout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <>
+      <Navbar toggleSidebar={toggleSidebar} />
+      <div style={{ display: 'flex', minHeight: 'calc(100vh - 56px)' }}>
+        {/* Sidebar */}
+        <Sidebar isOpen={isSidebarOpen} />
+        
+        {/* Main Content Area */}
+        <div style={{ width: "100%", overflowX: "hidden" }}>
+          <Outlet /> 
+        </div>
+      </div>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      { path: "/video/:id", element: <VideoPlayer /> },
+      { path: "/channel", element: <Channel /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
